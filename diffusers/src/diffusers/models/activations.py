893b9898a7137791397ce0f974a530beef4615fb
Gabriel Odai Afotey <gabrielafotey@gmail.com>
2025-04-24 12:48:39 +0000

    Initial commit

--- diffusers/src/diffusers/models/activations.py
+++ diffusers/src/diffusers/models/activations.py
@@ -1,6 +1,7 @@
 from torch import nn
 
 
+
 def get_activation(act_fn):
     if act_fn in ["swish", "silu"]:
         return nn.SiLU()
@@ -8,5 +9,9 @@ def get_activation(act_fn):
         return nn.Mish()
     elif act_fn == "gelu":
         return nn.GELU()
+    elif act_fn == "relu":
+        return nn.ReLU()
+    elif act_fn == "leaky_relu":
+        return nn.LeakyReLU(negative_slope=0.01)
     else:
         raise ValueError(f"Unsupported activation function: {act_fn}")
